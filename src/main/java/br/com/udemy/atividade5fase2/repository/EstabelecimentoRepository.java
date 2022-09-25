0bab1645fa715a4f6d70b9657518c6c9a8264236
Gabriel <gabrielmd908@gmail.com>
2022-09-25 17:25:12 -0300

    estabelecimento

--- src/main/java/br/com/udemy/atividade5fase2/repository/EstabelecimentoRepository.java
+++ src/main/java/br/com/udemy/atividade5fase2/repository/EstabelecimentoRepository.java
@@ -2,6 +2,9 @@ package br.com.udemy.atividade5fase2.repository;
 
 import br.com.udemy.atividade5fase2.model.Estabelecimento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, Integer> {
+
+    List<Estabelecimento> findByNome(String estabelecimento);
 }
